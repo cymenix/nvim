0f9e1a005cbdc235f4424080ed23d8e06d673ebb
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-22 11:18:27 +0200

    fix(which-key): remove deprecated option

--- config/ux/shortcuts/which-key/default.nix
+++ config/ux/shortcuts/which-key/default.nix
@@ -8,7 +8,6 @@
       plugins = {
         which-key = {
           enable = true;
-          ignoreMissing = true;
           registrations = {
             "<Tab>" = "Next buffer";
             "<S-Tab>" = "Previous buffer";
