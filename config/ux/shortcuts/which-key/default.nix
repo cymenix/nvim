c6771906078584f835aebf8a89d8ff16d1c4662c
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-23 17:52:43 +0200

    chore(ux): use old which-key version

--- config/ux/shortcuts/which-key/default.nix
+++ config/ux/shortcuts/which-key/default.nix
@@ -1,4 +1,24 @@
-{...}: {
+{
+  nixpkgs,
+  system,
+  ...
+}: let
+  pkgs = import nixpkgs {
+    inherit system;
+    overlays = [
+      (final: prev: {
+        vimPlugins.which-key-nvim = prev.vimPlugins.which-key-nvim.overrideAttrs (oldAttrs: {
+          src = prev.fetchFromGitHub {
+            owner = oldAttrs.src.owner;
+            repo = oldAttrs.src.repo;
+            rev = "0539da005b98b02cf730c1d9da82b8e8edb1c2d2"; # v2.1.0
+            hash = "sha256-gc/WJJ1s4s+hh8Mx8MTDg8pGGNOXxgKqBMwudJtpO4Y=";
+          };
+        });
+      })
+    ];
+  };
+in {
   programs = {
     nixvim = {
       opts = {
@@ -8,6 +28,7 @@
       plugins = {
         which-key = {
           enable = true;
+          package = pkgs.vimPlugins.which-key-nvim;
           registrations = {
             "<Tab>" = "Next buffer";
             "<S-Tab>" = "Previous buffer";
