20f1ba28fb3d6a920e9a98db926fae3567cefc1b
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-21 18:05:03 +0200

    chore(java): update
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- config/development/languages/java/nvim-jdtls/default.nix
+++ config/development/languages/java/nvim-jdtls/default.nix
@@ -2,9 +2,7 @@
   config,
   pkgs,
   ...
-}: let
-  jdtls = pkgs.jdt-language-server;
-in {
+}: {
   programs = {
     nixvim = {
       extraPlugins = [pkgs.vimPlugins.nvim-jdtls];
@@ -27,15 +25,8 @@ in {
                     capabilities = capabilities,
                     cmd = {
                       "jdtls",
-                      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-                      '-Dosgi.bundles.defaultStartLevel=4',
-                      '-Declipse.product=org.eclipse.jdt.ls.core.product',
                       '-Dlog.protocol=true',
                       '-Dlog.level=ALL',
-                      '-Xmx1g',
-                      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-                      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-                      '--add-modules=ALL-SYSTEM',
                       "-configuration",
                       vim.fs.normalize("${config.home.homeDirectory}/.cache/jdtls/config"),
                       "-data", vim.fs.root(0, {".git", "mvnw", "gradlew"})
