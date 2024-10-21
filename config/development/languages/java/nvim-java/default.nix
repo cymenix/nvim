7e8a6a944a438f193c0956484f0745cb4f1b28b7
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-21 13:14:52 +0200

    chore(java): update

--- config/development/languages/java/nvim-java/default.nix
+++ config/development/languages/java/nvim-java/default.nix
@@ -3,10 +3,10 @@
   nvim-java = pkgs.vimUtils.buildVimPlugin rec {
     name = owner;
     src = pkgs.fetchFromGitHub {
-      inherit owner;
+      owner = "clemenscodes";
       repo = name;
-      rev = "b3174e41ab51867123d8663eced53b33f1548522";
-      hash = "sha256-dKn4DaaacRRf9VrgN9TiTnxdqKnlmwCsr51DyAAAEeY=";
+      rev = "cafcb850a7ee2d05a055ac524e8b0db07e39cb74";
+      hash = "sha256-EbGePQKQavt4vN9dJx9iXGdjmY20QwD3qC/p/msQX9A=";
     };
   };
   nvim-java-core = pkgs.vimUtils.buildVimPlugin rec {
@@ -63,28 +63,9 @@
       hash = "sha256-SB+gmBfF3AKZyktOmPaR9CRyTyCYz2jlrxi+jgBI/Eo=";
     };
   };
-  sprint-boot-nvim = pkgs.vimUtils.buildVimPlugin rec {
-    name = "spring-boot.nvim";
-    src = pkgs.fetchFromGitHub {
-      inherit owner;
-      repo = name;
-      rev = "218c0c26c14d99feca778e4d13f5ec3e8b1b60f0";
-      hash = "sha256-5mzAr+VS5RGLi5e+ZohrlVHUzPa+6JwEWi4cslKPMNA=";
-    };
-  };
 in {
   programs = {
     nixvim = {
-      extraPackages = with pkgs; [
-        jdt-language-server
-        lombok
-        vscode-extensions.vscjava.vscode-java-debug
-        vscode-extensions.vscjava.vscode-java-test
-        vscode-extensions.vscjava.vscode-java-pack
-        vscode-extensions.vscjava.vscode-maven
-        vscode-extensions.vscjava.vscode-gradle
-        spring-boot-cli
-      ];
       extraPlugins = with pkgs.vimPlugins; [
         nvim-java
         nvim-java-core
@@ -93,11 +74,7 @@ in {
         nvim-java-test
         neotest-jdtls
         lua-async
-        sprint-boot-nvim
         nvim-jdtls
-        mason-nvim
-        mason-tool-installer-nvim
-        mason-lspconfig-nvim
         nui-nvim
       ];
       extraConfigLuaPost =
@@ -105,19 +82,6 @@ in {
         lua
         */
         ''
-          require("mason").setup({
-            registries = {
-              'github:nvim-java/mason-registry',
-              'github:mason-org/mason-registry',
-            },
-            ui = {
-              icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-              }
-            }
-          })
           require('java').setup({
           	--  list of file that exists in root of the project
           	root_markers = {
@@ -146,11 +110,6 @@ in {
           		enable = true,
           	},
 
-          	jdk = {
-          		-- install jdk using mason.nvim
-          		auto_install = false,
-          	},
-
           	notifications = {
           		-- enable 'Configuring DAP' & 'DAP configured' messages on start up
           		dap = true,
@@ -174,12 +133,6 @@ in {
           		-- Set following property value to false to disable the notification if
           		-- you know what you are doing
           		duplicate_setup_calls = true,
-
-          		-- nvim-java checks if nvim-java/mason-registry is added correctly to
-          		-- mason.nvim plugin.
-          		-- IF it's not registered correctly, an error will be thrown and nvim-java
-          		-- will stop setup
-          		invalid_mason_registry = false,
           	},
           })
           require('lspconfig').jdtls.setup({})
