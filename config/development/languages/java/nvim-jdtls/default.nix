030f8544cb20b3b4a1725c689f06b90a40275161
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-22 11:57:35 +0200

    chore(java): update
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- config/development/languages/java/nvim-jdtls/default.nix
+++ config/development/languages/java/nvim-jdtls/default.nix
@@ -11,7 +11,13 @@
           event = ["FileType"];
           pattern = "java";
           callback = {
-            __raw =
+            __raw = let
+              javaExt = "${pkgs.vscode-extensions.vscjava}";
+              sharePath = "share/vscode/extensions";
+              jarGlob = "server/*.jar";
+              debugger = "${javaExt.vscode-java-debug}/${sharePath}/vscjava.vscode-java-debug/${jarGlob}";
+              tester = "${javaExt.vscode-java-test}/${sharePath}/vscjava.vscode-java-test/${jarGlob}";
+            in
               /*
               lua
               */
@@ -22,6 +28,10 @@
                   local client_capabilities = vim.lsp.protocol.make_client_capabilities()
                   local capabilities = cmp_nvim_lsp.default_capabilities(client_capabilities)
                   local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+                  local bundles = {
+                    vim.fn.glob("${debugger}", 1)
+                  }
+                  vim.list_extend(bundles, vim.split(vim.fn.glob("${tester}", 1), "\n"))
                   local config = {
                     capabilities = capabilities,
                     cmd = {
@@ -41,10 +51,131 @@
                       vim.fs.normalize("${config.home.homeDirectory}/.cache/jdtls") .. "/" .. workspace_dir
                     },
                     root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"}),
+                    init_options = {
+                      bundles = bundles
+                    }
                   }
                   jdtls.start_or_attach(config)
                 end
               '';
+          };
+        }
+      ];
+      plugins = {
+        which-key = {
+          settings = {
+            spec = [
+              {
+                __unkeyed-1 = "<leader>cj";
+                group = "+Java";
+              }
+              {
+                __unkeyed-1 = "<leader>cji";
+                desc = "Organize imports";
+              }
+              {
+                __unkeyed-1 = "<leader>cjrev";
+                desc = "Create a variable from value at cursor/selection";
+              }
+              {
+                __unkeyed-1 = "<leader>cjreva";
+                desc = "Create a variable for all occurrences from value at cursor/selection";
+              }
+              {
+                __unkeyed-1 = "<leader>cjrec";
+                desc = "Create a constant from the value at cursor/selection";
+              }
+              {
+                __unkeyed-1 = "<leader>cjreca";
+                desc = "Create a constant from the value at cursor/selection";
+              }
+              {
+                __unkeyed-1 = "<leader>cjrem";
+                desc = "Create a method from the value at cursor/selection";
+              }
+              {
+                __unkeyed-1 = "<leader>cjtc";
+                desc = "Test the current class in the buffer";
+              }
+              {
+                __unkeyed-1 = "<leader>cjtm";
+                desc = "Test the nearest method in the buffer";
+              }
+            ];
+          };
+        };
+      };
+      keymaps = [
+        {
+          action = ":lua require('jdtls').organize_imports()<CR>";
+          key = "<leader>cji";
+          mode = "n";
+          options = {
+            desc = "Organize imports";
+            silent = true;
+          };
+        }
+        {
+          action = ":lua require('jdtls').extract_variable()<CR>";
+          key = "<leader>cjrev";
+          mode = "n";
+          options = {
+            desc = "Create a variable from value at cursor/selection";
+            silent = true;
+          };
+        }
+        {
+          action = ":lua require('jdtls').extract_variable(true)<CR>";
+          key = "<leader>cjreva";
+          mode = "n";
+          options = {
+            desc = "Create a variable for all occurrences from value at cursor/selection";
+            silent = true;
+          };
+        }
+        {
+          action = ":lua require('jdtls').extract_constant()<CR>";
+          key = "<leader>cjrec";
+          mode = "n";
+          options = {
+            desc = "Create a constant from the value at cursor/selection";
+            silent = true;
+          };
+        }
+        {
+          action = ":lua require('jdtls').extract_constant(true)<CR>";
+          key = "<leader>cjreca";
+          mode = "n";
+          options = {
+            desc = "Create a constant for all occurrences from the value at cursor/selection";
+            silent = true;
+          };
+        }
+        {
+          action = ":lua require('jdtls').extract_method()<CR>";
+          key = "<leader>cjrem";
+          mode = "n";
+          options = {
+            desc = "Create a method from the value at cursor/selection";
+            silent = true;
+          };
+        }
+        {
+          action = ":lua require('jdtls').test_class()<CR>";
+          key = "<leader>cjtc";
+          mode = "n";
+          options = {
+            desc = "Test the current class in the buffer";
+            silent = true;
+          };
+        }
+        {
+          action = ":lua require('jdtls').test_nearest_method()<CR>";
+          key = "<leader>cjtm";
+          mode = "n";
+          options = {
+            desc = "Test the nearest method in the buffer";
+            silent = true;
           };
         }
       ];
