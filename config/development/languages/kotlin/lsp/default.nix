430989678597db3575d30c93fbf33a279373b3f5
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-23 23:23:59 +0200

    chore(nvim): update
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- config/development/languages/kotlin/lsp/default.nix
+++ config/development/languages/kotlin/lsp/default.nix
@@ -2,17 +2,39 @@
   programs = {
     nixvim = {
       extraPlugins = [pkgs.vimPlugins.nvim-jdtls];
-      plugins = {
-        lsp = {
-          servers = {
-            kotlin-language-server = {
-              enable = true;
-              autostart = true;
-              filetypes = ["kotlin"];
-            };
+      autoCmd = [
+        {
+          event = ["FileType"];
+          pattern = "kotlin";
+          callback = {
+            __raw =
+              /*
+              lua
+              */
+              ''
+                function()
+                local cmp_nvim_lsp = require("cmp_nvim_lsp")
+                local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+                local capabilities = cmp_nvim_lsp.default_capabilities(client_capabilities)
+                  rquire('lspconfig').kotlin_language_server.setup{
+                    capabilities = capabilities,
+                    root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
+                    cmd = { "kotlin-language-server" },
+                    settings = {
+                      kotlin = {
+                        compiler = {
+                          jvm = {
+                            target = "1.8";
+                          }
+                        };
+                      };
+                    }
+                  }
+                end
+              '';
           };
-        };
-      };
+        }
+      ];
     };
   };
 }
