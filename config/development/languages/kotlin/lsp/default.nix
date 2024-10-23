822379d0daf2bce73105771249ad1b4c146ccfad
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-23 23:59:44 +0200

    chore(nvim): update
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- config/development/languages/kotlin/lsp/default.nix
+++ config/development/languages/kotlin/lsp/default.nix
@@ -16,14 +16,10 @@
                   local cmp_nvim_lsp = require("cmp_nvim_lsp")
                   local client_capabilities = vim.lsp.protocol.make_client_capabilities()
                   local capabilities = cmp_nvim_lsp.default_capabilities(client_capabilities)
-                  local util = require('lspconfig/util')
                   require('lspconfig').kotlin_language_server.setup{
                     capabilities = capabilities,
                     -- root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew", "gradle.lock", "settings.gradle.kts" }),
                     cmd = { "kotlin-language-server" },
-                    init_options = {
-                       storagePath = util.path.join(vim.env.XDG_DATA_HOME, "nvim-data"),
-                    };
                     settings = {
                       kotlin = {
                         compiler = {
