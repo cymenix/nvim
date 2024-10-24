9471a7914d7552c1a7f7ee9b5aabfa57bf284879
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-24 14:32:19 +0200

    chore(java): update
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- config/development/languages/kotlin/lsp/default.nix
+++ config/development/languages/kotlin/lsp/default.nix
@@ -2,28 +2,28 @@
   programs = {
     nixvim = {
       plugins = {
-        lsp = {
-          servers = {
-            kotlin-language-server = {
-              enable = true;
-              autostart = true;
-              cmd = ["${pkgs.kotlin-language-server}/bin/kotlin-language-server"];
-              filetypes = ["kotlin" "kt"];
-              settings = {
-                kotlin = {
-                  compiler = {
-                    target = {
-                      jvm = "1.8";
-                    };
-                  };
-                  languageServer = {
-                    path = "${pkgs.kotlin-language-server}/bin/kotlin-language-server";
-                  };
-                };
-              };
-            };
-          };
-        };
+        # lsp = {
+        #   servers = {
+        #     kotlin-language-server = {
+        #       enable = true;
+        #       autostart = true;
+        #       cmd = ["${pkgs.kotlin-language-server}/bin/kotlin-language-server"];
+        #       filetypes = ["kotlin" "kt"];
+        #       settings = {
+        #         kotlin = {
+        #           compiler = {
+        #             target = {
+        #               jvm = "1.8";
+        #             };
+        #           };
+        #           languageServer = {
+        #             path = "${pkgs.kotlin-language-server}/bin/kotlin-language-server";
+        #           };
+        #         };
+        #       };
+        #     };
+        #   };
+        # };
         conform-nvim = {
           settings = {
             formatters_by_ft = {
@@ -32,44 +32,39 @@
           };
         };
       };
-      extraPlugins = with pkgs.vimPlugins; [kotlin-vim];
-      # extraConfigLuaPost = ''
-      #   require('lspconfig').kotlin_language_server.setup{
-      #     root_dir = vim.fs.root(0, {".git", "gradle.lock"}),
-      #     cmd = { "kotlin-language-server" }
-      #   }
-      # '';
-      # autoCmd = [
-      #   {
-      #     event = ["FileType"];
-      #     pattern = "kotlin";
-      #     callback = {
-      #       __raw =
-      #         /*
-      #         lua
-      #         */
-      #         ''
-      #           function()
-      #             require('lspconfig').kotlin_language_server.setup{
-      #               cmd = { "${pkgs.kotlin-language-server}/bin/kotlin-language-server" },
-      #               settings = {
-      #                 kotlin = {
-      #                   compiler = {
-      #                     target = {
-      #                       jvm = "1.8"
-      #                     }
-      #                   },
-      #                   languageServer = {
-      #                     path = "${pkgs.kotlin-language-server}/bin/kotlin-language-server"
-      #                   }
-      #                 }
-      #               }
-      #             }
-      #           end
-      #         '';
-      #     };
-      #   }
-      # ];
+      autoCmd = [
+        {
+          event = ["FileType"];
+          pattern = "kotlin";
+          callback = {
+            __raw =
+              /*
+              lua
+              */
+              ''
+                function()
+                  local lsp = require('lspconfig')
+                  lsp.kotlin_language_server.setup{
+                    cmd = { "${pkgs.kotlin-language-server}/bin/kotlin-language-server" },
+                    root_dir = lsp.util.root_pattern("gradle.lock", ".git"),
+                    settings = {
+                      kotlin = {
+                        compiler = {
+                          target = {
+                            jvm = "1.8"
+                          }
+                        },
+                        languageServer = {
+                          path = "${pkgs.kotlin-language-server}/bin/kotlin-language-server"
+                        }
+                      }
+                    }
+                  }
+                end
+              '';
+          };
+        }
+      ];
     };
   };
 }
