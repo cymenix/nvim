72bfdcbf1e774de52459c40322fe995eafc0dd2e
Clemens Horn <clemens.horn@mni.thm.de>
2024-10-24 12:24:20 +0200

    chore(java): update
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- config/development/languages/kotlin/lsp/default.nix
+++ config/development/languages/kotlin/lsp/default.nix
@@ -1,30 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   programs = {
     nixvim = {
-      autoCmd = [
-        {
-          event = ["FileType"];
-          pattern = "kotlin";
-          callback = {
-            __raw =
-              /*
-              lua
-              */
-              ''
-                function()
-                  local cmp_nvim_lsp = require("cmp_nvim_lsp")
-                  local client_capabilities = vim.lsp.protocol.make_client_capabilities()
-                  local capabilities = cmp_nvim_lsp.default_capabilities(client_capabilities)
-                  require('lspconfig').kotlin_language_server.setup{
-                    capabilities = capabilities,
-                    root_dir = vim.fs.root(0, {".git", "gradle.lock"}),
-                    cmd = { "kotlin-language-server" }
-                  }
-                end
-              '';
-          };
+      extraPlugins = with pkgs.vimPlugins; [kotlin-vim];
+      extraConfigLuaPost = ''
+        require('lspconfig').kotlin_language_server.setup{
+          root_dir = vim.fs.root(0, {"gradle.lock"}),
+          cmd = { "kotlin-language-server" }
         }
-      ];
+      '';
       plugins = {
         conform-nvim = {
           settings = {
@@ -34,6 +17,26 @@
           };
         };
       };
+      # autoCmd = [
+      #   {
+      #     event = ["FileType"];
+      #     pattern = "kotlin";
+      #     callback = {
+      #       __raw =
+      #         /*
+      #         lua
+      #         */
+      #         ''
+      #           function()
+      #             require('lspconfig').kotlin_language_server.setup{
+      #               root_dir = vim.fs.root(0, {"gradle.lock"}),
+      #               cmd = { "kotlin-language-server" }
+      #             }
+      #           end
+      #         '';
+      #     };
+      #   }
+      # ];
     };
   };
 }
